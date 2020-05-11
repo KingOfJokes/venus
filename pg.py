91828b8640d953943eea8b13b1df1cd318bad765
KingOfJokes <47939642+KingOfJokes@users.noreply.github.com>
2020-05-11 14:12:40 +0800

    Add files via upload

---
+++ pg.py
@@ -0,0 +1,323 @@
+import datagenerate as dgp
+import dg
+import numpy as np
+import time
+import math
+import system
+import os
+import pandas as pd
+import json
+import matplotlib.pyplot as plt
+
+dataset = {
+	'close0050':[],
+	'openn0050':[],
+	'adjopenn':[],
+	'adjclose':[],
+	'openn':[],
+	'high':[],
+	'low':[],
+	'close':[],
+	'volume':[],
+	'turnover':[],
+	'outstand':[],
+	'mktc':[],
+	'pe':[],
+	'pb':[],
+	'cdy':[],
+	'fore':[],
+	'it':[],
+	'dler':[],
+	'foreratio':[],
+	'itratio':[],
+	'dlerratio':[],
+	'managerratio':[],
+	'mortratio':[],
+	'finb':[],
+	'shortb':[],
+	'fsratio':[],
+	'rsv9':[],
+	'k9':[],
+	'd9':[],
+	'ema12':[],
+	'ema26':[],
+	'dif':[],
+	'dem':[],
+	'bar':[],
+	'ma5':[],
+	'ma10':[],
+	'ma20':[],
+	'ma60':[],
+	'ma120':[],
+	'ma240':[],
+	'vma5':[],
+	'vma10':[],
+	'vma20':[],
+	'vma60':[],
+	'vma120':[],
+	'vma240':[],
+	'ma13':[],
+	'ma21':[],
+	'ma55':[],
+	'totalers':[],
+	'ers1':[],
+	'ers1to5':[],
+	'ers5to10':[],
+	'ers10to15':[],
+	'ers15to20':[],
+	'ers20to30':[],
+	'ers30to40':[],
+	'ers40to50':[],
+	'ers50to100':[],
+	'ers100to200':[],
+	'ers200to400':[],
+	'ers400to600':[],
+	'ers600to800':[],
+	'ers800to1000':[],
+	'ers1000plus':[],
+	'ratio1':[],
+	'ratio1to5':[],
+	'ratio5to10':[],
+	'ratio10to15':[],
+	'ratio15to20':[],
+	'ratio20to30':[],
+	'ratio30to40':[],
+	'ratio40to50':[],
+	'ratio50to100':[],
+	'ratio100to200':[],
+	'ratio200to400':[],
+	'ratio400to600':[],
+	'ratio600to800':[],
+	'ratio800to1000':[],
+	'above1000ratio':[],
+	'totalerscombo':[],
+	'to50ratio':[],
+	'to50ratiocombo':[],
+	'to100ratio':[],
+	'to100ratiocombo':[],
+	'above400ratio':[],
+	'above400ratiocombo':[],
+	'above1000ratiocombo':[],
+	'per1000pluser':[],
+	'ers400plus':[],
+	'per400pluser':[],
+	'other400':[],
+	'other400combo':[],
+	'publish':[],
+	'rev':[],
+	'lyrev':[],
+	'yoy':[],
+	'mom':[],
+	'accrev':[],
+	'accrevyoy':[],
+	'yoycombo':[],
+	'momcombo':[],
+	'accrevyoycombo':[],
+	'breakhigh':[],
+	'cash':[],
+	'conta':[],
+	'ar':[],
+	'inv':[],
+	'ca':[],
+	'ppe':[],
+	'nca':[],
+	'totala':[],
+	'shortdebt':[],
+	'contl':[],
+	'ap':[],
+	'cl':[],
+	'longdebt':[],
+	'ncl':[],
+	'totall':[],
+	'equitycap':[],
+	'quarrev':[],
+	'gross':[],
+	'opex':[],
+	'rdex':[],
+	'opgain':[],
+	'incomebeftax':[],
+	'eps':[],
+	'roe':[],
+	'grossmargin':[],
+	'opmargin':[],
+	'ibtmargin':[],
+	'iatmargin':[],
+	'opexmargin':[],
+	'rdexmargin':[],
+	'curratio':[],
+	'quickratio':[],
+	'debtratio':[],
+	'arday':[],
+	'invday':[],
+	'apday':[],
+	'grossmargindy':[],
+	'grossmargindq':[],
+	'opmargindy':[],
+	'opmargindq':[],
+	'iatmargindy':[],
+	'iatmargindq':[],
+	'ibtmargindy':[],
+	'ibtmargindq':[],
+	'ardaydy':[],
+	'ardaydq':[],
+	'invdaydy':[],
+	'invdaydq':[],
+	'apdaydy':[],
+	'apdaydq':[],
+	'debtratiody':[],
+	'debtratiodq':[],
+	'quickratiody':[],
+	'quickratiodq':[],
+	'roedy':[],
+	'roedq':[]
+}
+def createfolder(directory):
+    try:
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+    except OSError:
+        print ('Error: Creating directory. ' +  directory)
+
+def pos_operate(npdata,number,sign):
+	size_r = len(npdata)
+	size_c = len(npdata[0])
+	num = np.full((size_r,size_c),number)
+	comp = npdata-num
+	if sign == '>':
+		boo = comp>0
+	elif sign == '>=':
+		boo = comp>=0
+	elif sign == '=':
+		boo = comp==0
+	elif sign == '<':
+		boo = comp<0
+	elif sign == '<=':
+		boo = comp<=0    
+	return boo
+
+def uni(npdata,thresh,func):
+    temp = np.roll(npdata,1,axis=0)
+    if func == '>':
+    	boo = np.where(temp > thresh,npdata,np.nan)
+    elif func == '<':
+    	boo = np.where(temp < thresh,npdata,np.nan)
+    boo = boo/boo
+    return boo
+
+def newstock_adj(npdata):
+    c2 = np.full_like(npdata,1)
+    for k in range(0,6):
+        temp = np.roll(npdata,k,axis=0)
+        temp = temp/temp
+        c2 = c2*temp
+    return c2
+def sigmoid(ndarr):
+	return (1/(1+np.exp(-ndarr)))
+
+def thresh(ori_data,day_start,day_end,thr,func):
+	temp_arr = np.full((len(ori_data),len(ori_data[0])),1)
+	for i in range(day_start,day_end+1):
+		temp = np.roll(ori_data,i,axis = 0)
+		temp_boo = pos_operate(temp,thr,func)
+		temp_arr = temp_arr*temp_boo
+	return temp_arr
+
+def showdata(backtest_inf,title,sample,fd,folder_time):
+	output_dict = {}
+	output_dict['Annual Return'] = '%.2f'%(100*backtest_inf['annualyield'])+'%'
+	output_dict['Annual Std'] = '%.2f'%(100*backtest_inf['annualstd'])+'%'
+	output_dict['Sharpe'] = '%.2f'%backtest_inf['sharpe']
+	output_dict['Turnover rate'] = '%.2f'%(100*backtest_inf['turnover'])+'%'
+	output_dict['Longest DD'] = backtest_inf['longestdd']
+	output_dict['Maximum DD'] = '%.2f'%(100*backtest_inf['maximumdd'])+'%'
+
+	print('Annual Return:','%.2f'%(100*backtest_inf['annualyield']),'%')
+	print('Annual Std.','%.2f'%(100*backtest_inf['annualstd']),'%')
+	print('Sharpe:','%.2f'%backtest_inf['sharpe'])
+	print('Turnover rate:','%.2f'%(100*backtest_inf['turnover']),'%')
+	print('Longest DD:',backtest_inf['longestdd'])
+	print('Maximum DD:','%.2f'%(100*backtest_inf['maximumdd']),'%')
+
+	plt.title('Backtest Summary - '+title+' '+sample)
+	plt.plot(backtest_inf['dates'], backtest_inf['assets'], label = 'Strategy')
+	#plt.plot(backtest_inf['dates'], backtest_inf['historicalhigh'], label = 'Cap')
+	plt.legend(loc='upper left')
+	plt.savefig('./backtest/'+fd+'/'+folder_time+'/'+sample)
+	plt.show()
+	plt.close()
+	return output_dict
+
+def oldbt(positions,removeh,valid,fd,ftime,wannaknow):
+	result = system.backtest(positions,removeh,valid)
+	backtest_inf = result[0]
+	valid_inf = result[1]
+	distribution = result[2]
+
+	output_json = {}
+	output_json['Filter'] = []
+	output_json['Result-IS'] = []
+	output_json['Result-OS'] = []
+	filter_str = wannaknow
+	filter_dict = {'String':filter_str}
+	output_json['Filter'].append(filter_dict)
+
+	print('In-sample')
+	json_is = showdata(backtest_inf,'','IS',fd,ftime)
+	print('Out-sample')
+	json_os = showdata(valid_inf,'','OS',fd,ftime)
+
+	output_json['Result-IS'].append(json_is)
+	output_json['Result-OS'].append(json_os)
+
+	with open('./backtest/'+fd+'/'+ftime+'/Readme.json', 'w') as outfile:
+		for js in output_json:
+			outfile.write(json.dumps(js))
+			outfile.write(json.dumps(output_json[js]))
+			outfile.write("\n") 
+
+	print('Description of each bet')
+	distribution_pd = pd.DataFrame(distribution)
+	distribution_np = np.asarray(distribution)
+	print(distribution_pd.describe())
+	plt.hist(distribution_np,bins=50,range=(-0.2,0.2)) 
+	plt.title("Distribution")
+	plt.savefig('./backtest/'+fd+'/'+ftime+'/distribution')
+	plt.show()
+	return [json_is,json_os]
+
+def newbt(positions,removeh,valid,ftime,wannaknow):
+	result = system.backtest2(positions,removeh,valid)
+	backtest_inf = result[0]
+	valid_inf = result[1]
+	distribution = result[2]
+
+	output_json = {}
+	output_json['Filter'] = []
+	output_json['Result-IS'] = []
+	output_json['Result-OS'] = []
+	filter_str = wannaknow
+	filter_dict = {'String':filter_str}
+	output_json['Filter'].append(filter_dict)
+
+	print('In-sample')
+	json_is = showdata(backtest_inf,'DashBoard','IS',ftime)
+	print('Out-sample')
+	json_os = showdata(valid_inf,'DashBoard','OS',ftime)
+
+	output_json['Result-IS'].append(json_is)
+	output_json['Result-OS'].append(json_os)
+
+	with open('./backtest/'+ftime+'/Readme.json', 'w') as outfile:
+		for js in output_json:
+			outfile.write(json.dumps(js))
+			outfile.write(json.dumps(output_json[js]))
+			outfile.write("\n") 
+
+	print('Description of each bet')
+	distribution_pd = pd.DataFrame(distribution)
+	distribution_np = np.asarray(distribution)
+	print(distribution_pd.describe())
+	plt.hist(distribution_np,bins=50,range=(-0.2,0.2)) 
+	plt.title("Distribution")
+	plt.savefig('./backtest/'+ftime+'/distribution')
+	plt.show()
--- system.py
+++ system.py
@@ -10,12 +10,20 @@ import dg
 from pandas.plotting import register_matplotlib_converters
 register_matplotlib_converters()
 
+uni = []
+def universe(send):
+	global uni
+	uni = send
+
 def filter(d,f):
 	#purify
-	#print(type(dataset['close']))
+	print(uni)
+	for u in uni:
+		pass
+
 	for i in range(0,6): #新股上市前5天容易失真
 		if math.isnan((dataset['close'])[d-i,f]) == True: 
-			return 0
+			return np.nan
 
 	for i in range(1,2):
 		#secure meaningful compare
@@ -25,7 +33,7 @@ def filter(d,f):
 		if dataset['k9'][d-i,f] - dataset['d9'][d-i,f] < 0 :
 			return 0
 	#Those which buy
-	weight = 1	
+	weight = 1 #按照要投入的金額比例調整這裡
 	return weight
 dataset = {
 	'close0050':[],
@@ -189,7 +197,9 @@ dataset = {
 	'roedy':[],
 	'roedq':[]
 }
-load_needed = ['close','adjopenn','adjclose'] #這邊要把底下filter要呼叫的資料庫打進來
+load_needed = ['openn','close','adjopenn','adjclose'] #這邊要把底下filter要呼叫的資料庫打進來
+for u in uni:
+	load_needed.append(u[0])
 def dataimport():
 	for data_name in load_needed:
 		dataset[data_name] = np.load("surface/"+data_name+".npy",allow_pickle = True)
@@ -204,9 +214,9 @@ def datacreate():
 
 def filter2(d,f,ifs,wei):
 	#purify
-	for i in range(0,6):
+	for i in range(1,6):
 		if math.isnan((dataset['close'])[d-i,f]) == True: 
-			return 0
+			return np.nan
 
 	def value(df1,df2,a,sign):
 		if sign == '+':
@@ -217,6 +227,23 @@ def filter2(d,f,ifs,wei):
 			return dataset[df1][d-a,f]*dataset[df2][d-a,f]
 		elif sign == '/':
 			return dataset[df1][d-a,f]/dataset[df2][d-a,f]
+
+	for u in uni:
+		if u[1] == '>':
+			if value(u[0],u[0],1,'+') <= 2*u[2]:
+				return np.nan
+		elif u[1] == '>=':
+			if value(u[0],u[0],1,'+') < 2*u[2]:
+				return np.nan
+		elif u[1] == '=':
+			if value(u[0],u[0],1,'+') != 2*u[2]:
+				return np.nan
+		elif u[1] == '<=':
+			if value(u[0],u[0],1,'+') > 2*u[2]:
+				return np.nan
+		elif u[1] == '<':
+			if value(u[0],u[0],1,'+') >= 2*u[2]:
+				return np.nan
 
 	#Those which don't buy
 	for if_sit in ifs:
@@ -240,9 +267,30 @@ def filter2(d,f,ifs,wei):
 				if value(if_sit[0],if_sit[2],i,if_sit[1]) != if_sit[4] :
 					return 0
 	#Those which buy
-	weight = wei
-	return weight
+	if wei == 1:
+		weight = wei
+	else:
+		weisplit = wei.split('-')
+		figure = weisplit[0]
+		mode = weisplit[1]
 
+	if figure == 'price':
+		weight = dataset['openn'][d,f]
+	elif 'signal' in figure:
+		index = int(figure.split('signal')[1])-1
+		if_sit2 = ifs[index]
+		weight = value(if_sit2[0],if_sit2[2],1,if_sit2[1])
+
+	if mode == '' or mode == 'ori': #default mode為ori
+		weight = weight
+	elif mode == 'opp':
+		weight = -weight
+	elif mode == 'inverse':
+		if weight != 0:
+			weight = 1/weight
+	elif mode == 'abs':
+		weight = abs(weight)
+	return weight
 
 #Block2:import data
 tdlist = list(np.load("surface/tdlist.npy",allow_pickle = True))
@@ -251,10 +299,12 @@ dataset['close0050'] = np.load("surface/close0050.npy",allow_pickle = True)
 dataset['openn0050'] = np.load("surface/openn0050.npy",allow_pickle = True)
 
 def dataquery(input_ifs):
-	load_needed = ['close','adjopenn','adjclose']
+	load_needed = ['openn','close','adjopenn','adjclose']
 	for if_sit in input_ifs:
 		load_needed.append(if_sit[0])
 		load_needed.append(if_sit[2])
+	for u in uni:
+		load_needed.append(u[0])
 	load_needed = list(set(load_needed))
 
 	for data_name in load_needed:
@@ -267,14 +317,14 @@ def showinf(field,date,firm):
 #Block3:Create Positions
 
 #temp_position_matrix = np.asarray([0 for x in range(len(firmlist))])
-def rank(ndarr):
+def rankize(posi):
 	output = []
-	for i in range(len(ndarr)):
-		temp = [0 for i in range(len(ndarr[i]))]
-		ranking = np.argsort(ndarr[i])
+	for i in range(len(posi)):
+		temp = [0 for i in range(len(posi[i]))]
+		ranking = np.argsort(posi[i])
 		start = 1
 		for ind in range(len(ranking)):
-			if ndarr[i,ranking[ind]] == 0:
+			if posi[i,ranking[ind]] == 0:
 				pass
 			else:
 				temp[ranking[ind]] = start
@@ -283,34 +333,50 @@ def rank(ndarr):
 	output = np.asarray(output)
 	return(output)
 	
-def fade(nparray,day): #day should be more than 2
+def longize(posit,day): #day should be more than 2
 	#給定array, 吐出一個把每個非0元至少自動持續day天，要從後面往前，不然會更新錯
-	upperbound =len(nparray)-1 
+	posi = posit.copy()
+	upperbound =len(posi)-1 
 	for d in range(upperbound,-1,-1):
-		for f in range(len(nparray[d])):
-			if nparray[d,f] != 0 and day >= 2:
+		for f in range(len(posi[d])):
+			if posi[d,f] != 0 and day >= 2:
 				for i in range(d+1,min(upperbound,d+day)):
-					if nparray[i,f] == 0:
-						nparray[i,f] = nparray[d,f]
-	return nparray
+					if posi[i,f] == 0:
+						posi[i,f] = posi[d,f]
+	return posi
 
-def normalize(nparray):
-	summation = 0
+def neutralize(posit):
+	posi = posit.copy()
+	adj_matrix = []
+	for d in range(len(posi)):
+		adj = np.nanmean(posi[d])
+		temp = [adj for i in range(len(posi[d]))]
+		adj_matrix.append(temp)
+	adj_matrix = np.asarray(adj_matrix)
+	posi = posi - adj_matrix
+	return posi
+
+def normalize(posit):
 	#validnumber = 0
-	for i in nparray:
-		if math.isnan(i)==False:
-			summation = summation + i
-			#validnumber = validnumber + 1
-	if summation != 0:
-		nparray = nparray/abs(summation)
-	return nparray
-def parrel(nparray,delta):
-	return nparray-delta
+	posi = posit.copy()
+	posisum = np.nansum(abs(posi),axis = 1)
+	posisum = np.tile(posisum,(len(posi[0]),1)).T
+	posi = posi/posisum
+	return posi
 
-def positionize(removehead):
+def shareize(posit):
+	output = []
+	posi = posit.copy()
+	pricetag = dataset['openn']
+	for pd in range(len(posi)):
+		temp = [posi[pd,f]/pricetag[pd,f] for f in range(len(posi[pd]))]
+		output.append(temp)
+	output = np.asarray(output)
+	return output
+
+def positionize():
 	position_matrix = []
-	for d in range(removehead,len(tdlist)):
-
+	for d in range(1,len(tdlist)):
 		temp_position_matrix = []
 		for f in range(len(firmlist)):
 			temp_position_matrix.append(filter(d,f))
@@ -319,13 +385,12 @@ def positionize(removehead):
 	position_matrix = np.asarray(position_matrix)
 	return position_matrix
 
-def positionize2(removehead,input_ifs,wei):
+def positionize2(input_ifs,wei):
 	position_matrix = []
-	for d in range(removehead,len(tdlist)):
+	for d in range(1,len(tdlist)):
 		temp_position_matrix = []
 		for f in range(len(firmlist)):
 			temp_position_matrix.append(filter2(d,f,input_ifs,wei))
-		temp_position_matrix = normalize(np.asarray(temp_position_matrix))
 		position_matrix.append(temp_position_matrix)
 	position_matrix = np.asarray(position_matrix)
 	return position_matrix
@@ -338,13 +403,14 @@ def positionize2(removehead,input_ifs,wei):
 #Block4a:Initialize
 #benchmark = np.load("C:/Users/Ryan/Desktop/mercury/surface/close0050.npy",allow_pickle = True)
 
+'''
 def innerp(checkar1,checkar2):
 	result = 0
 	for i in range(len(checkar1)):
 		if math.isnan(checkar1[i])==False and math.isnan(checkar2[i])==False:
 			result = result + checkar1[i]*checkar2[i]
 	return result
-
+'''
 #Block4b:Backtest
 def backtest(position_matrix,removehead,valid):
 	ini = 100
@@ -352,31 +418,54 @@ def backtest(position_matrix,removehead,valid):
 	return_rec = []
 	hishighs = [ini]
 	dropdowns = []
-	backtestdates = [tdlist[removehead-1]]
+	turnovers = []
+	backtestdates = [tdlist[0]]
 	totaloperate = 0
 	hishigh = ini
+	eachperf = []
 
-	for d in range(1,len(position_matrix) - valid):
-		pricetag_bef = dataset['adjopenn'][d+removehead-1,0:]
-		pricetag_aft = dataset['adjopenn'][d+removehead,0:]
+	for d in range(removehead+1,len(tdlist)):
+		#pricetag_bef = dataset['adjopenn'][d+removehead-1,0:]
+		#pricetag_aft = dataset['adjopenn'][d+removehead,0:]
+		pricetag_bef = dataset['adjopenn'][d-1,0:]
+		pricetag_aft = dataset['adjopenn'][d,0:]
 
 		bef_pos = position_matrix[d-1]
-		aft_pos = position_matrix[d]
-		operate = bef_pos - aft_pos #前減後
+		aft_pos = position_matrix[d-0]
+		operate = aft_pos - bef_pos #前減後
 
-		if innerp(pricetag_bef,bef_pos)!= 0:
-			dailyreturn = innerp(pricetag_aft,bef_pos)/innerp(pricetag_bef,bef_pos)
+		for f in range(len(bef_pos)):
+			if math.isnan(pricetag_bef[f]) == False:
+				eff = np.sign(bef_pos[f])*((pricetag_aft[f]-pricetag_bef[f])/pricetag_bef[f])
+			else:
+				eff = np.nan
+			if eff > -0.4 and eff < 0.4:
+				eachperf.append(eff)
+			elif math.isnan(eff) == False:
+				print('error',eff,'d',tdlist[d],'f',firmlist[f],pricetag_aft[f],pricetag_bef[f])
+				bef_pos[f] = 0
+
+		if np.nansum(pricetag_bef*bef_pos)!= 0:
+			dailyreturn = 1+((np.nansum(pricetag_aft*bef_pos)-np.nansum(pricetag_bef*bef_pos))/np.nansum(pricetag_bef*abs(bef_pos)))
 		else:
 			dailyreturn = 1
-
+		
+		turnover = np.nansum(pricetag_aft*abs(operate))
+		scale = np.nansum(pricetag_bef*abs(bef_pos))
+		if scale != 0:
+			turnover_ratio = turnover/scale
+		else:
+			turnover_ratio = 0
+		turnovers.append(turnover_ratio)
+		
 		ini = ini*dailyreturn
 		assets_rec.append(ini)
 		return_rec.append(dailyreturn)
-		backtestdates.append(tdlist[d+removehead])
+		backtestdates.append(tdlist[d])
 
-		if assets_rec[d] > hishigh:
-			hishigh = assets_rec[d]
-		dropdown = (hishigh-assets_rec[d])/hishigh
+		if assets_rec[d-removehead-1] > hishigh:
+			hishigh = assets_rec[d-removehead-1]
+		dropdown = (hishigh-assets_rec[d-removehead-1])/hishigh
 		dropdowns.append(dropdown)
 		hishighs.append(hishigh)
 
@@ -384,11 +473,12 @@ def backtest(position_matrix,removehead,valid):
 	return_rec = np.asarray(return_rec)-1
 	backtestdates = np.asarray(backtestdates)
 	hishighs = np.asarray(hishighs)
+	turnovers = np.asarray(turnovers)
 	maxduration = 0
 	duration = 0
 	maxdd = 0
 
-	for dd in dropdowns:
+	for dd in dropdowns[0:len(assets_rec)-valid]:
 		if dd > 0:
 			duration = duration + 1
 		elif dd == 0:
@@ -398,64 +488,110 @@ def backtest(position_matrix,removehead,valid):
 		if dd > maxdd:
 			maxdd = dd
 
-	annualgeo = ((assets_rec[len(assets_rec)-1]/assets_rec[0])**(240/len(return_rec))) - 1
-	annualstd = np.std(return_rec)*(240**0.5)
-	sharpe = annualgeo/annualstd
+	assets_rec_in = assets_rec[0:len(assets_rec)-valid]
+	assets_rec_out = assets_rec[len(assets_rec)-valid:len(assets_rec)]
+	assets_rec_out = assets_rec_out/assets_rec_out[0]
+	return_rec_in = return_rec[0:len(return_rec)-valid]
+	return_rec_out = return_rec[len(return_rec)-valid:len(return_rec)]
+	backtestdates_in = backtestdates[0:len(backtestdates)-valid]
+	backtestdates_out = backtestdates[len(backtestdates)-valid:len(backtestdates)]
+	turnovers_in = turnovers[0:len(turnovers)-valid]
+	turnovers_out = turnovers[len(turnovers)-valid:len(turnovers)]
+
+	annualgeo_in = ((assets_rec_in[len(assets_rec_in)-1]/assets_rec_in[0])**(240/len(return_rec_in))) - 1
+	annualstd_in = np.std(return_rec_in)*(240**0.5)
+	sharpe_in = annualgeo_in/annualstd_in
+
+	annualgeo_out = ((assets_rec_out[len(assets_rec_out)-1]/assets_rec_out[0])**(240/len(return_rec_out))) - 1
+	annualstd_out = np.std(return_rec_out)*(240**0.5)
+	sharpe_out = annualgeo_out/annualstd_out
+
 	backtest_dict = {
-		'assets':assets_rec,
-		'return':return_rec,
-		'dates':backtestdates,
-		'annualyield':annualgeo,
-		'annualstd':annualstd,
-		'sharpe':sharpe,
+		'assets':assets_rec_in,
+		'return':return_rec_in,
+		'dates':backtestdates_in,
+		'annualyield':annualgeo_in,
+		'annualstd':annualstd_in,
+		'sharpe':sharpe_in,
 		'longestdd':maxduration,
 		'maximumdd':maxdd,
-		'historicalhigh':hishighs}
-	return backtest_dict
+		'turnover':np.nanmean(turnovers_in)
+		#'historicalhigh':hishighs
+		}
+	validfy_dict = {
+		'assets':assets_rec_out,
+		'return':return_rec_out,
+		'dates':backtestdates_out,
+		'annualyield':annualgeo_out,
+		'annualstd':annualstd_out,
+		'sharpe':sharpe_out,
+		'longestdd':0,
+		'maximumdd':0,
+		'turnover':np.nanmean(turnovers_out)
+		#'historicalhigh':hishighs
+		}
 
-def validfy(position_matrix,removehead,valid):
+	return [backtest_dict,validfy_dict,eachperf]
+
+def backtest2(position,removehead,valid):
 	ini = 100
 	assets_rec = [ini]
 	return_rec = []
 	hishighs = [ini]
 	dropdowns = []
-	backtestdates = [tdlist[removehead-1]]
+	turnovers = []
+	backtestdates = [tdlist[0]]
 	totaloperate = 0
 	hishigh = ini
+	eachperf = []
 
-	for d in range(len(position_matrix) - valid,len(temp_position_matrix)):
-		pricetag_bef = dataset['adjopenn'][d+removehead-1,0:]
-		pricetag_aft = dataset['adjopenn'][d+removehead,0:]
+	pricetag = dataset['adjopenn']
+	pricetag_yest = np.roll(pricetag,1,axis = 0) 
+	position_yest = np.roll(position,1,axis = 0)
+	operate = position - position_yest
+	scale = np.nansum(pricetag_yest*np.abs(position_yest),axis = 1)
+	turn = np.nansum(pricetag*np.abs(operate),axis = 1)
+	turnover = turn/scale
+	cost = position_yest*pricetag_yest
+	result = position_yest*pricetag
+	dailyreturn = np.nansum(result,axis = 1)/np.nansum(cost,axis = 1)
+	eachbet = (result-cost)/cost
 
-		bef_pos = position_matrix[d-1]
-		aft_pos = position_matrix[d]
-		operate = bef_pos - aft_pos #前減後
-
-		if innerp(pricetag_bef,bef_pos)!= 0:
-			dailyreturn = innerp(pricetag_aft,bef_pos)/innerp(pricetag_bef,bef_pos)
+	for d in range(removehead+1,len(position)):
+		if np.isinf(turnover[d]) == False and np.isnan(turnover[d]) == False:
+			turnovers.append(turnover[d])
 		else:
-			dailyreturn = 1
+			turnovers.append(0)
 
-		ini = ini*dailyreturn
+		ini = ini*dailyreturn[d]
 		assets_rec.append(ini)
-		return_rec.append(dailyreturn)
-		backtestdates.append(tdlist[d+removehead])
+		return_rec.append(dailyreturn[d])
+		backtestdates.append(tdlist[d])
 
-		if assets_rec[d] > hishigh:
-			hishigh = assets_rec[d]
-		dropdown = (hishigh-assets_rec[d])/hishigh
+		if assets_rec[d-removehead-1] > hishigh:
+			hishigh = assets_rec[d-removehead-1]
+		dropdown = (hishigh-assets_rec[d-removehead-1])/hishigh
 		dropdowns.append(dropdown)
 		hishighs.append(hishigh)
+
+	for d in range(1,len(eachbet)):
+		for f in range(len(eachbet[d])):
+			bet = eachbet[d,f]
+			if np.isnan(bet) == False:
+				eachperf.append(bet)
+			if np.abs(bet)>0.4:
+				print('error',bet,'d',tdlist[d],'f',firmlist[f],pricetag[d,f],pricetag_yest[d,f])
 
 	assets_rec = np.asarray(assets_rec)
 	return_rec = np.asarray(return_rec)-1
 	backtestdates = np.asarray(backtestdates)
 	hishighs = np.asarray(hishighs)
+	turnovers = np.asarray(turnovers)
 	maxduration = 0
 	duration = 0
 	maxdd = 0
 
-	for dd in dropdowns:
+	for dd in dropdowns[0:len(assets_rec)-valid]:
 		if dd > 0:
 			duration = duration + 1
 		elif dd == 0:
@@ -465,17 +601,47 @@ def validfy(position_matrix,removehead,valid):
 		if dd > maxdd:
 			maxdd = dd
 
-	annualgeo = ((assets_rec[len(assets_rec)-1]/assets_rec[0])**(240/len(return_rec))) - 1
-	annualstd = np.std(return_rec)*(240**0.5)
-	sharpe = annualgeo/annualstd
+	assets_rec_in = assets_rec[0:len(assets_rec)-valid]
+	assets_rec_out = assets_rec[len(assets_rec)-valid:len(assets_rec)]
+	assets_rec_out = assets_rec_out/assets_rec_out[0]
+	return_rec_in = return_rec[0:len(return_rec)-valid]
+	return_rec_out = return_rec[len(return_rec)-valid:len(return_rec)]
+	backtestdates_in = backtestdates[0:len(backtestdates)-valid]
+	backtestdates_out = backtestdates[len(backtestdates)-valid:len(backtestdates)]
+	turnovers_in = turnovers[0:len(turnovers)-valid]
+	turnovers_out = turnovers[len(turnovers)-valid:len(turnovers)]
+
+	annualgeo_in = ((assets_rec_in[len(assets_rec_in)-1]/assets_rec_in[0])**(240/len(return_rec_in))) - 1
+	annualstd_in = np.std(return_rec_in)*(240**0.5)
+	sharpe_in = annualgeo_in/annualstd_in
+
+	annualgeo_out = ((assets_rec_out[len(assets_rec_out)-1]/assets_rec_out[0])**(240/len(return_rec_out))) - 1
+	annualstd_out = np.std(return_rec_out)*(240**0.5)
+	sharpe_out = annualgeo_out/annualstd_out
+
 	backtest_dict = {
-		'assets':assets_rec,
-		'return':return_rec,
-		'dates':backtestdates,
-		'annualyield':annualgeo,
-		'annualstd':annualstd,
-		'sharpe':sharpe,
+		'assets':assets_rec_in,
+		'return':return_rec_in,
+		'dates':backtestdates_in,
+		'annualyield':annualgeo_in,
+		'annualstd':annualstd_in,
+		'sharpe':sharpe_in,
 		'longestdd':maxduration,
 		'maximumdd':maxdd,
-		'historicalhigh':hishighs}
-	return backtest_dict
+		'turnover':np.nanmean(turnovers_in)
+		#'historicalhigh':hishighs
+		}
+	validfy_dict = {
+		'assets':assets_rec_out,
+		'return':return_rec_out,
+		'dates':backtestdates_out,
+		'annualyield':annualgeo_out,
+		'annualstd':annualstd_out,
+		'sharpe':sharpe_out,
+		'longestdd':0,
+		'maximumdd':0,
+		'turnover':np.nanmean(turnovers_out)
+		#'historicalhigh':hishighs
+		}
+
+	return [backtest_dict,validfy_dict,eachperf]
